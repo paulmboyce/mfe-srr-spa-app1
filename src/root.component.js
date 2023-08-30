b068f1e6f25357f63070b1636da061898eaaf395
paulmboyce <paul.boyce@goobito.com>
2023-08-30 14:03:27 +0100

    feature(app events);add button click event to prove hydration

--- src/root.component.js
+++ src/root.component.js
@@ -1,7 +1,13 @@
 export default function Root(props) {
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log("The link was clicked.");
+  };
+
   return (
     <section>
-      <h1>{props.name} is mounted!</h1>
+      <h1>{props.name} is mounted with events!</h1>
+      <button onClick={handleClick}>Click me</button>
     </section>
   );
 }
