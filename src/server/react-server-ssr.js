e18100870cd2f76edc601a55a6faa799a102cffa
paulmboyce <paul.boyce@goobito.com>
2023-09-11 12:01:34 +0100

    refactor(SSR):remove App indirection

--- src/server/react-server-ssr.js
+++ src/server/react-server-ssr.js
@@ -2,44 +2,45 @@
 require("@babel/register");
 const express = require("express");
 const React = require("react");
+import Root from "../root.component"; // Import your main React component
 const {
   renderToString,
   renderToStaticNodeStream,
 } = require("react-dom/server");
-var cors = require("cors");
+const cors = require("cors");
 const { ServerStyleSheet } = require("styled-components");
-
-/** !IMPORTANT App sjhould be a named export,not a default export */
-const { App } = require("./App");
-
-console.log("APP works >>>>>>>> 2");
 
 const app = express();
 app.use(cors());
-// Expose our webpack bundle for client side hydration
-app.use(express.static("dist"));
+app.use(express.static("dist")); // Expose our webpack bundle for client side hydration
+//app.use(express.static("public")); // OPTIONAL: Serve static assets like CSS, JS, and images
 
 // eslint-disable-next-line no-undef
 const port = process.env.PORT || 3000;
 
-//app.use(express.static("public")); // Serve static assets like CSS, JS, and images
-
-app.get("/", (req, res, next) => {
+/**
+ * Render html + css as static string
+ */
+app.get("/ssr", (req, res, next) => {
   const sheet = new ServerStyleSheet();
-  const html = renderToString(sheet.collectStyles(<App />));
+  const html = renderToString(
+    sheet.collectStyles(<Root name="SSR Root [html]" />)
+  );
   const styleTags = sheet.getStyleTags();
-  // const html = renderToString(<App />); // no styling
   res.send(styleTags.concat(html));
 });
 
-app.get("/html/stream", (req, res, next) => {
+/**
+ * Render html + css as stream (single-spa prefers a stream for performance)
+ */
+app.get("/ssr/stream", (req, res, next) => {
   const sheet = new ServerStyleSheet();
-  const jsx = sheet.collectStyles(<App />);
+  const jsx = sheet.collectStyles(<Root name="SSR Root [stream]" />);
+
   const stream = sheet.interleaveWithNodeStream(renderToStaticNodeStream(jsx));
-  // var stream = renderToStaticNodeStream(<App />); // no styling
   stream.pipe(res);
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`React SSR Server started on port ${port}`);
 });
